1b46911dea9956e121b9386a30f81bb366f01641
F ReyeSFT <f.reyesft@gmail.com>
2023-09-05 11:57:31 -0500

    los iconos se añaden a el array de objetos

--- src/components/CardProyect.jsx
+++ src/components/CardProyect.jsx
@@ -1,5 +1,5 @@
 import { Divider, Card, CardHeader, CardBody, Image, CardFooter, Link } from '@nextui-org/react'
-import { BiLogoJavascript, BiLogoHtml5, BiLogoCss3 } from 'react-icons/bi'
+import { BiLogoJavascript, BiLogoHtml5, BiLogoCss3, BiLogoReact } from 'react-icons/bi'
 
 export default function CardProyect () {
   const portfolio = [
@@ -9,20 +9,22 @@ export default function CardProyect () {
       link: 'https://freyesft.github.io/calculadora/',
       linkGitHub: 'https://github.com/FreyesFT/calculadora',
       iconos: [
-        <BiLogoHtml5 className='text-[#E34F26]' key={1} />,
-        <BiLogoCss3 key={2} className='text-[#2965F1]' />,
-        <BiLogoJavascript key={3} className='text-[#f7df1e]' />]
+        <BiLogoHtml5 className='text-[#E34F26] drop-shadow-md' key={1} />,
+        <BiLogoCss3 key={2} className='text-[#2965F1] drop-shadow-md' />,
+        <BiLogoJavascript key={3} className='text-[#f7df1e] drop-shadow-md' />]
     },
     {
       title: 'Triqui',
       image: '../../images/triqui-web.png',
       link: 'https://triqui-wine.vercel.app',
-      linkGitHub: 'https://github.com/FreyesFT/calculadora',
+      linkGitHub: 'https://github.com/FreyesFT/triqui',
       iconos: [
-        <BiLogoHtml5 className='text-[#E34F26]' key={1} />,
-        <BiLogoCss3 key={2} className='text-[#2965F1]' />,
-        <BiLogoJavascript key={3} className='text-[#f7df1e]' />]
-    },
+        <BiLogoHtml5 className='text-[#E34F26] drop-shadow-md' key={1} />,
+        <BiLogoCss3 key={2} className='text-[#2965F1] drop-shadow-md' />,
+        <BiLogoJavascript key={3} className='text-[#f7df1e] drop-shadow-md' />,
+        <BiLogoReact key={4} className='text-[#61DBFB] drop-shadow-md' />
+      ]
+    }
   ]
   return (
     <>
@@ -60,7 +62,6 @@ export default function CardProyect () {
                   </Link>
                 </CardFooter>
               </Card>
-
             ))
           }
     </>

21e2d5122b19561eea427e226973a046c7d6c793
F ReyeSFT <f.reyesft@gmail.com>
2023-09-14 22:36:05 -0500

    cambio contenido del texto aboutMeCard

--- src/components/AboutMeCard.jsx
+++ src/components/AboutMeCard.jsx
@@ -2,9 +2,10 @@ export default function AboutMeCard () {
   return (
     <>
       <p className='text-secondary text-sm mt-3 text-about-me md:w-1/2 md:text-base'>
-        Desarrollador web apasionado que fusiona creatividad, diseño y precisión en el
-        desarrollo para lograr soluciones excepcionales. Disfruto superando desafíos y explorando nuevas
-        tecnologías.
+        Desarrollador de Software especializado en front-end con bases solidas en el uso de tecnologias como;
+        JavaScript, HTML, CSS, React, Next js y manejo de control de versiones con
+        GitHub. Con ganas de trabajar, crecer y aportar mis conocimientos para resolver las tareas con creatividad,
+        diseño y precisión. 💻
       </p>
     </>
   )

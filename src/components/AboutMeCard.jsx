8fd5d34d922bdf3b883ac2c40d0a77c766b2769e
Fernando Reyez Pinto <f.reyeft@gmail.com>
2023-10-23 10:26:38 -0500

    update about me

--- src/components/AboutMeCard.jsx
+++ src/components/AboutMeCard.jsx
@@ -1,12 +1,13 @@
-export default function AboutMeCard () {
+export default function AboutMeCard() {
   return (
     <>
-      <p className='text-secondary text-sm mt-3 text-about-me md:w-1/2 md:text-base'>
-        Desarrollador de Software especializado en front-end con bases solidas en el uso de tecnologias como;
-        JavaScript, HTML, CSS, React, Next js y manejo de control de versiones con
-        GitHub. Con ganas de trabajar, crecer y aportar mis conocimientos para resolver las tareas con creatividad,
-        diseño y precisión. 💻
+      <p className="text-secondary text-sm mt-3 text-about-me md:w-1/2 md:text-base">
+        Desarrollador de Software especializado en front-end con experiencia en
+        el uso de tecnologias como; JavaScript, HTML, CSS, React, Next js,
+        Angular y GitHub. Con ganas de trabajar, crecer y aportar mis
+        conocimientos para resolver las tareas con creatividad, diseño y
+        precisión. 💻
       </p>
     </>
-  )
+  );
 }

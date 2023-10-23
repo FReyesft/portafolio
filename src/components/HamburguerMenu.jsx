f69da27d95d6c7a6e9e9551ebd9d65a9c0b018a1
Fernando Reyez Pinto <f.reyeft@gmail.com>
2023-10-23 10:22:12 -0500

    close menu clicking link

--- src/components/HamburguerMenu.jsx
+++ src/components/HamburguerMenu.jsx
@@ -1,58 +1,52 @@
-import { motion } from 'framer-motion'
-import Menu from './Menu'
-import { useState } from 'react'
-
-export default function HamburguerMenu () {
-  const [clicked, setClicked] = useState(false)
+import { motion } from "framer-motion";
+import Menu from "./Menu";
+import { useState } from "react";
+import { useContext } from "react";
+export default function HamburguerMenu() {
+  const [clicked, setClicked] = useState(false);
   return (
     <>
       <div
         onClick={() => setClicked(!clicked)}
         className={`${
-          clicked ? '' : ''
-        } ${'hover:cursor-pointer fixed top-4 left-4 right-4 w-8 h-8 border-2 rounded-full border-black flex flex-col-reverse justify-center items-center z-40 md:w-12 md:h-12'}`}
+          clicked ? "" : ""
+        } ${"hover:cursor-pointer fixed top-4 left-4 right-4 w-8 h-8 border-2 rounded-full border-black flex flex-col-reverse justify-center items-center z-40 md:w-12 md:h-12"}`}
       >
         <motion.span
           animate={{
-            rotate: clicked ? '45deg' : '0deg',
-            y: clicked ? '-5px' : '0px'
+            rotate: clicked ? "45deg" : "0deg",
+            y: clicked ? "-5px" : "0px",
           }}
           transition={{ duration: 0.3 }}
           whileHover={{
-            scale: 1.1
+            scale: 1.1,
           }}
-          className='m-[2px] w-4 md:w-6 h-[2px] bg-black rounded-sm'
+          className="m-[2px] w-4 md:w-6 h-[2px] bg-black rounded-sm"
         />
         <motion.span
           animate={{
-            rotate: clicked ? '-45deg' : '0deg',
-            opacity: clicked ? 0 : 1
+            rotate: clicked ? "-45deg" : "0deg",
+            opacity: clicked ? 0 : 1,
           }}
           transition={{ duration: 0.3 }}
           whileHover={{
-            scale: 1.1
+            scale: 1.1,
           }}
-          className='m-[2px] w-4 md:w-6 h-[2px] bg-black rounded-sm'
+          className="m-[2px] w-4 md:w-6 h-[2px] bg-black rounded-sm"
         />
         <motion.span
           animate={{
-            rotate: clicked ? '-45deg' : '0deg',
-            y: clicked ? '7px' : '0px'
+            rotate: clicked ? "-45deg" : "0deg",
+            y: clicked ? "7px" : "0px",
           }}
           transition={{ duration: 0.3 }}
           whileHover={{
-            scale: 1.1
+            scale: 1.1,
           }}
-          className='m-[2px] w-4 md:w-6 h-[2px] bg-black rounded-sm'
+          className="m-[2px] w-4 md:w-6 h-[2px] bg-black rounded-sm"
         />
       </div>
-      {clicked
-        ? (
-          <Menu />
-          )
-        : (
-            ''
-          )}
+      {clicked ? <Menu /> : ""}
     </>
-  )
+  );
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,52 +1,93 @@
-import { Link } from '@nextui-org/react'
-import { motion } from 'framer-motion'
-export default function Menu () {
+import { Link } from "@nextui-org/react";
+import { motion } from "framer-motion";
+import { useState } from "react";
+export default function Menu() {
+  const [clicked, setClicked] = useState(true);
   const item = {
     visible: {
       opacity: 1,
       x: 0,
-      color: '#fff'
+      color: "#fff",
     },
 
-    hidden: { opacity: 0, x: '-300px' }
-  }
+    hidden: { opacity: 0, x: "-300px" },
+  };
   const variants = {
     visible: {
       opacity: 1,
       x: 0,
       transition: {
         staggerChildren: 0.1,
-        delayChildren: 0.2
-      }
+        delayChildren: 0.2,
+      },
     },
     hidden: {
-      x: '-200px',
-      opacity: 1
-    }
-  }
+      x: "-200px",
+      opacity: 1,
+    },
+  };
   return (
     <>
-      <motion.ul
-        variants={variants}
-        initial='hidden'
-        animate='visible'
-        transition={{ duration: 0.5 }}
-        className={`${'flex justify-center flex-col top-0 left-0 right-0 h-full w-full p-4 container_menu fixed z-30 items-center'}`}
-      >
-        <motion.li variants={item}>
-          <Link className='text-secondary text-3xl m-6 font-bold' href='#homeSection' underline='hover'>Home</Link>
-        </motion.li>
-        <motion.li variants={item}>
-          <Link className='text-secondary text-3xl m-6 font-bold' href='#aboutMeSection' underline='hover'>Sobre mi</Link>
-        </motion.li>
-        <motion.li variants={item}>
-          <Link className='text-secondary text-3xl m-6 font-bold' href='#contactSection' underline='hover'>Contacto</Link>
-        </motion.li>
-        <motion.li variants={item}>
-          <Link className='text-secondary text-3xl m-6 font-bold' href='#portfolioSection' underline='hover'>Portafolio</Link>
-        </motion.li>
-      </motion.ul>
-
+      {clicked ? (
+        <motion.ul
+          variants={variants}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 0.5 }}
+          className={`${"flex justify-center flex-col top-0 left-0 right-0 h-full w-full p-4 container_menu fixed z-30 items-center"}`}
+        >
+          <motion.li variants={item}>
+            <Link
+              onClick={() => {
+                setClicked(!clicked);
+              }}
+              className="text-secondary text-3xl m-6 font-bold"
+              href="#homeSection"
+              underline="hover"
+            >
+              Home
+            </Link>
+          </motion.li>
+          <motion.li variants={item}>
+            <Link
+              onClick={() => {
+                setClicked(!clicked);
+              }}
+              className="text-secondary text-3xl m-6 font-bold"
+              href="#aboutMeSection"
+              underline="hover"
+            >
+              Sobre mi
+            </Link>
+          </motion.li>
+          <motion.li variants={item}>
+            <Link
+              onClick={() => {
+                setClicked(!clicked);
+              }}
+              className="text-secondary text-3xl m-6 font-bold"
+              href="#contactSection"
+              underline="hover"
+            >
+              Contacto
+            </Link>
+          </motion.li>
+          <motion.li variants={item}>
+            <Link
+              onClick={() => {
+                setClicked(!clicked);
+              }}
+              className="text-secondary text-3xl m-6 font-bold"
+              href="#portfolioSection"
+              underline="hover"
+            >
+              Portafolio
+            </Link>
+          </motion.li>
+        </motion.ul>
+      ) : (
+        ""
+      )}
     </>
-  )
+  );
 }

89c65f60102f0022b4e2cdb5965dd61d0d232e41
Fernando Reyez Pinto <f.reyeft@gmail.com>
2023-10-30 08:18:19 -0500

    format to single quote

--- src/components/ContactMeCard.jsx
+++ src/components/ContactMeCard.jsx
@@ -1,15 +1,15 @@
-import { Card, CardBody, Button, Input, CardFooter } from "@nextui-org/react";
+import { Card, CardBody, Button, Input, CardFooter } from '@nextui-org/react';
 import {
   BiLogoLinkedin,
   BiSolidSend,
   BiLogoWhatsapp,
   BiLogoGithub,
-} from "react-icons/bi";
-import Link from "next/link";
+} from 'react-icons/bi';
+import Link from 'next/link';
 
-import { useRef } from "react";
-import emailjs from "@emailjs/browser";
-import toast from "react-hot-toast";
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import toast from 'react-hot-toast';
 export default function App() {
   const form = useRef();
 
@@ -17,18 +17,18 @@ export default function App() {
     e.preventDefault();
     emailjs
       .sendForm(
-        "service_mlwjxmr",
-        "template_kvuv1g4",
+        'service_mlwjxmr',
+        'template_kvuv1g4',
         form.current,
-        "cqhm42JADe_4NAsfh"
+        'cqhm42JADe_4NAsfh'
       )
       .then(
         () => {
-          toast.success("Tu mensaje ha sido enviado correctamente");
+          toast.success('Tu mensaje ha sido enviado correctamente');
         },
         (error) => {
           console.log(error.text);
-          toast.error("Tu mensaje no ha podido ser enviado");
+          toast.error('Tu mensaje no ha podido ser enviado');
         }
       );
   };
@@ -36,10 +36,12 @@ export default function App() {
     <Card
       isBlurred
       className="border-none bg-background/60 dark:bg-default-100/50 w-full max-w-[710px] mt-3"
-      shadow="sm"
-    >
+      shadow="sm">
       <CardBody>
-        <form className="flex flex-col" ref={form} onSubmit={sendEmail}>
+        <form
+          className="flex flex-col"
+          ref={form}
+          onSubmit={sendEmail}>
           <Input
             name="user_name"
             variant="underlined"
@@ -74,8 +76,7 @@ export default function App() {
             type="submit"
             color="success"
             variant="shadow"
-            startContent={<BiSolidSend />}
-          >
+            startContent={<BiSolidSend />}>
             Enviar
           </Button>
         </form>
@@ -83,22 +84,19 @@ export default function App() {
           <Link
             href="https://www.linkedin.com/in/fernando-reyez-pinto-2a19b9260/"
             target=" _blank"
-            className="p-1 mr-2 ml-2 hover:scale-125 shadow-md"
-          >
+            className="p-1 mr-2 ml-2 hover:scale-125 shadow-md">
             <BiLogoLinkedin className="text-3xl text-[#0e76a8]" />
           </Link>
           <Link
             href="https://wa.me/+573142325833/?text=Enviame un mensaje"
             target=" _blank"
-            className="p-1 mr-2 ml-2 hover:scale-125  shadow-md"
-          >
+            className="p-1 mr-2 ml-2 hover:scale-125  shadow-md">
             <BiLogoWhatsapp className="text-3xl text-[#25D366] " />
           </Link>
           <Link
             href="https://github.com/FReyesft"
             target=" _blank"
-            className="p-1 mr-2 ml-2 hover:scale-125 shadow-md"
-          >
+            className="p-1 mr-2 ml-2 hover:scale-125 shadow-md">
             <BiLogoGithub className="text-3xl text-[#24292e]" />
           </Link>
         </CardFooter>
